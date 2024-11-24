a7b13ab92888261ab0815be032cc31de1b6df40b
Grady Alexis <grady@evermos.com>
2024-11-24 11:15:52 +0700

    fix: thumbnail

--- src/constants/meta.js
+++ src/constants/meta.js
@@ -1,7 +1,7 @@
 export const META = {
   DEFAULT_TITLE: 'Situs Jual Beli Online Smartphone Terlengkap, Mudah & Aman | TokoMulyo',
   DEFAULT_DESCRIPTION: 'Situs jual beli smartphone terlengkap dengan berbagai pilihan toko online terpercaya. Belanja smartphone dengan mudah dan menyenangkan di TokoMulyo. Pengiriman cepat.',
-  DEFAULT_IMAGE: 'https://rollingglory.com/images/meta_description.png?v=3',
+  DEFAULT_IMAGE: './public/thumbnail.png',
 }
 
 export default META
