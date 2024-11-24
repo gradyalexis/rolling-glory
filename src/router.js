b8a6a5305e57ea92863f2235303b9c3597c8de71
Grady Alexis <grady@evermos.com>
2024-11-24 10:49:33 +0700

    fix: remove meta tag router

--- src/router.js
+++ src/router.js
@@ -6,29 +6,14 @@ const routes = [
   {
     path: '/',
     component: Home,
-    meta: {
-      title: 'Situs Jual Beli Online Smartphone Terlengkap, Mudah & Aman | TokoMulyo',
-      description: 'Situs jual beli smartphone terlengkap dengan berbagai pilihan toko online terpercaya. Belanja smartphone dengan mudah dan menyenangkan di TokoMulyo. Pengiriman cepat.',
-      image: 'https://example.com/thumbnail.jpg',
-    },
   },
   {
     path: '/item/:id',
     component: Item,
-    meta: {
-      title: 'Xiaomi Mi A2 Lite',
-      description: '',
-      image: 'https://example.com/product-thumbnail.jpg',
-    },
   },
   {
     path: '/:catchAll(.*)',
     redirect: '/',
-    meta: {
-      title: 'Situs Jual Beli Online Smartphone Terlengkap, Mudah & Aman | TokoMulyo',
-      description: 'Situs jual beli smartphone terlengkap dengan berbagai pilihan toko online terpercaya. Belanja smartphone dengan mudah dan menyenangkan di TokoMulyo. Pengiriman cepat.',
-      image: 'https://example.com/thumbnail.jpg',
-    },
   },
 ]
 
