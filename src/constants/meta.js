bb171ed3238ec41dea746aaed342ad7acc7c40e1
Grady Alexis <grady@evermos.com>
2024-11-24 11:58:20 +0700

    fix: image thumbnail error

--- src/constants/meta.js
+++ src/constants/meta.js
@@ -1,7 +1,7 @@
 export const META = {
   DEFAULT_TITLE: 'Situs Jual Beli Online Smartphone Terlengkap, Mudah & Aman | TokoMulyo',
   DEFAULT_DESCRIPTION: 'Situs jual beli smartphone terlengkap dengan berbagai pilihan toko online terpercaya. Belanja smartphone dengan mudah dan menyenangkan di TokoMulyo. Pengiriman cepat.',
-  DEFAULT_IMAGE: './public/thumbnail.png',
+  DEFAULT_IMAGE: '/thumbnail.png',
 }
 
 export default META
