7d3a1f5abefa6db2b647e745002ccd858a71c2de
Grady Alexis <grady@evermos.com>
2024-11-24 12:14:46 +0700

    feat: add pwa

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite'
 import { BootstrapVueNextResolver } from 'bootstrap-vue-next'
@@ -13,6 +14,27 @@ export default defineConfig({
     vueDevTools(),
     Components({
       resolvers: [BootstrapVueNextResolver()],
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'TokoMulyo',
+        short_name: 'TM',
+        description: 'Situs jual beli smartphone terlengkap dengan berbagai pilihan toko online terpercaya. Belanja smartphone dengan mudah dan menyenangkan di TokoMulyo. Pengiriman cepat.',
+        theme_color: '#79B625',
+        icons: [
+          {
+            src: '/thumbnail-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/thumbnail-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
     }),
   ],
   resolve: {
