31a6053e70f778806860cb0442363e99c5063289
Grady Alexis <grady@evermos.com>
2024-11-24 11:48:24 +0700

    feat: add product badge in product detail

--- src/Item.vue
+++ src/Item.vue
@@ -1,47 +1,53 @@
 <template>
   <Breadcrumb class="mt-4 p-4" :product-name="product?.attributes?.name || ''" />
-  <div class="row mt-4 z-2">
-    <div class="col">
+  <div v-if="product" class="row mt-4 z-2">
+    <div class="position-relative d-flex align-items-center col">
       <img
-        :src="product?.attributes?.images[0] || ''"
+        :src="product.attributes.images[0] || ''"
         class="w-100 object-fit-md-contain mb-2"
         style="height: 460px;"
-        :alt="product?.attributes?.name || ''"
+        :alt="product.attributes.name || ''"
+      />
+      <ProductBadge
+        v-if="generateBadge(product.attributes)"
+        :variant="generateBadge(product.attributes)"
+        class="position-absolute"
+        style="top: 0; right: 0;"
       />
     </div>
     <div class="col z-2">
       <div class="fs-3 fw-semibold mb-2">
-        {{ product?.attributes?.name || '' }}
+        {{ product.attributes.name || '' }}
         <div class="d-flex align-items-center gap-2">
           <star-rating
             :active-color="['#F0D946','#F0D946']"
             :star-size="15"
-            :rating="product?.attributes?.rating || 0"
+            :rating="product.attributes.rating || 0"
             :show-rating="false"
             :round-start-rating="false"
             :padding="2"
             read-only
           />
           <div class="text-grey mt-2" style="font-size: 12px;">
-            {{ product?.attributes?.numOfReviews || 0 }} reviews
+            {{ product.attributes.numOfReviews || 0 }} reviews
           </div>
         </div>
       </div>
       <div class="d-flex gap-2">
         <img src="@/assets/point-big.png" class="object-fit-md-contain" />
         <div class="text-green fs-6 fw-bold" style="font-size: 12px;">
-          {{ product?.attributes?.points }} poins
+          {{ product.attributes.points }} poins
         </div>
         <div class="position-relative fs-6 fw-semibold text-grey-dark">
-          {{ productStock(product?.attributes?.stock) }}
+          {{ productStock(product.attributes.stock) }}
         </div>
       </div>
-      <div v-html="product?.attributes?.description || ''" class="mt-2" />
+      <div v-html="product.attributes.description || ''" class="mt-2" />
       <QuantityCounter class="mt-4" />
       <div class="d-flex mt-4 gap-3">
         <WishlistButton
-          :product-id="product?.id"
-          :variant="product?.attributes?.wishlist ? 'danger' : 'primary'"
+          :product-id="product.id"
+          :variant="product.attributes.wishlist ? 'danger' : 'primary'"
           size="lg"
           @wishlisted="fetchProduct"
         />
@@ -59,7 +65,7 @@
         </button>  
       </div>
     </div>
-    <DetailProduct class="z-2" :render="product?.attributes?.info || ''" />
+    <DetailProduct class="z-2" :render="product.attributes.info || ''" />
   </div>
 </template>
 
@@ -97,6 +103,22 @@ const productStock = (stock) => {
   }
 
   return `Stock < ${stock}`
+}
+
+const generateBadge = ({ isNew, rating, numOfReviews }) => {
+  if (isNew && rating >= 4 && numOfReviews > 25) {
+    return 'hot'
+  }
+
+  if (rating >= 4 && numOfReviews > 25) {
+    return 'best'
+  }
+
+  if (isNew) {
+    return 'new'
+  }
+
+  return null
 }
 
 onMounted(async () => {
